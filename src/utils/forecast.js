21873d1f06818d70038b7c6b65f845d5730a5c37
InfiniteDoom <tanzhex@gmail.com>
2020-05-29 01:58:44 +0800

    Added Humidity Info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.current.weather_descriptions[0]) {
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
         } else {
-            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity +"%" )
         }
     })
 
